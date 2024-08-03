0f2a04a438d7bc1ab16f056d43846dfd5370ab87
felipe.chagas <felypegt8@gmail.com>
2024-08-03 20:38:24 -0300

    13. Usando o Model Serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from cursos.models import Avaliacao, Curso
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True} # configuração write_only só para escrever, isso quer dizer que não vai ser possível fazer consulta com GET
+        }
+        
+        model = Avaliacao
+        
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        )
+        
+        
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo'
+        )
